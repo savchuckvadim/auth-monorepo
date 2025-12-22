e06abcc37c8bbff86b3c35734b8d18216fd92aed
Vadim Savchuk <savchuckvadim@gmail.com>
2025-12-22 21:33:14 +0300

    add dynamic

--- apps/front/app/auth/login/page.tsx
+++ apps/front/app/auth/login/page.tsx
@@ -1,11 +1,15 @@
 'use client';
-import { LoginForm } from "@/modules/processes";
 import { Card, CardHeader } from "@workspace/ui/components/card";
 import { CardTitle } from "@workspace/ui/components/card";
 import { CardDescription } from "@workspace/ui/components/card";
 import { CardContent } from "@workspace/ui/components/card";
+import dynamic from "next/dynamic";
 import Link from "next/link";
 
+const DynamicLoginForm = dynamic(() => import('@/modules/processes/auth/ui/LoginForm/LoginForm')
+    .then(mod => mod.LoginForm), {
+    ssr: false,
+});
 
 export default function Page() {
     return (
@@ -18,7 +22,7 @@ export default function Page() {
             </CardHeader>
             <CardContent>
 
-                <LoginForm />
+                <DynamicLoginForm />
                 <div className="text-right mt-4 flex items-center justify-between">
                     <p className="text-sm text-gray-500">Нет аккаунта?</p>
                     <Link href="/auth/register" className="text-sm text-blue-500 hover:text-blue-700 ml-2">
