b75f2bd14225bc3a42694dccb73d54ffe3bf1131
Vadim Savchuk <savchuckvadim@gmail.com>
2026-01-07 20:18:20 +0300

    add routing

---
+++ apps/front/modules/processes/routing/type/routing.type.ts
@@ -0,0 +1,18 @@
+export enum EnumRoutingPath {
+    HOME = '/network',
+    NETWORK = '/network',
+    CHATS = '/network/chats',
+    CHATS_LIST = '/network/chats/list',
+    CHATS_ITEM = '/network/chats/:id',
+    PEOPLE = '/network/people',
+    PEOPLE_ITEM = '/network/people/:id',
+    ME = '/network/me',
+
+    AUTH_LOGIN = '/auth/login',
+    AUTH_REGISTER = '/auth/register',
+    AUTH_CONFIRM = '/auth/confirm',
+    AUTH_START = '/auth/start',
+
+}
+
+
